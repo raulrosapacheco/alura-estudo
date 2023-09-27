579fa3cd5d14755c4ba727ec2a45632402cb45e0
Raul Rosa <rr0608@outlook.com>
2023-09-26 22:09:38 -0300

    voltando para trás

--- git-github/sistema-de-cadastro/app.js
+++ git-github/sistema-de-cadastro/app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicacao está se conectanto ao banco de dados")
-// mais um console log de teste:
-console.log("O paulo fez esse commit sem voce ver na aula!")
-console.log("Enviando para o GitHub")
