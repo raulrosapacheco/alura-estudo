7df0f663c0d1103b5f59a9d49c3006a1d48ed1c8
Raul Rosa <rr0608@outlook.com>
2023-09-26 21:55:50 -0300

    adicionando um console log

--- git-github/sistema-de-cadastro/app.js
+++ git-github/sistema-de-cadastro/app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao está se conectanto ao banco de dados")
 // mais um console log de teste:
-cpnsole.log("O paulo fez esse commit sem voce ver na aula!")
+console.log("O paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
