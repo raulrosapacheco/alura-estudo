c1cd36993bcfad7406ba1e5e50d79529f359f603
Raul Rosa <rr0608@outlook.com>
2023-09-26 21:53:37 -0300

    Update app.js

--- git-github/sistema-de-cadastro/app.js
+++ git-github/sistema-de-cadastro/app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao está se conectanto ao banco de dados")
+// mais um console log de teste:
+cpnsole.log("O paulo fez esse commit sem voce ver na aula!")
